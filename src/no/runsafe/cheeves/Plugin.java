ad56770472d25e39508a79c9b689004998d50628
Kruithne <kruithne@gmail.com>
2013-06-10 19:47:20 +0100

    Initialized new plugin Cheeves from template

---
+++ src/no/runsafe/cheeves/Plugin.java
@@ -0,0 +1,12 @@
+package no.runsafe.cheeves;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class Plugin extends RunsafePlugin
+{
+	@Override
+	protected void PluginSetup()
+	{
+		//addComponent(SomeComponent.class);
+	}
+}
