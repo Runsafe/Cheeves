c20c36b8bb19722a7a96c19093623ebc1d894aa6
Kruithne <kruithne@gmail.com>
2013-06-19 18:26:08 +0100

    Simplified code.

--- src/no/runsafe/cheeves/achievements/FleshWound.java
+++ src/no/runsafe/cheeves/achievements/FleshWound.java
@@ -3,18 +3,15 @@ package no.runsafe.cheeves.achievements;
 import no.runsafe.cheeves.Achievement;
 import no.runsafe.cheeves.AchievementHandler;
 import no.runsafe.cheeves.Achievements;
-import no.runsafe.framework.api.IOutput;
 import no.runsafe.framework.api.event.player.IPlayerDeathEvent;
-import no.runsafe.framework.minecraft.event.entity.RunsafeEntityDamageByEntityEvent;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerDeathEvent;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
 
 public class FleshWound extends Achievement implements IPlayerDeathEvent
 {
-	public FleshWound(AchievementHandler achievementHandler, IOutput output)
+	public FleshWound(AchievementHandler achievementHandler)
 	{
 		super(achievementHandler);
-		this.output = output;
 	}
 
 	@Override
@@ -38,22 +35,8 @@ public class FleshWound extends Achievement implements IPlayerDeathEvent
 	@Override
 	public void OnPlayerDeathEvent(RunsafePlayerDeathEvent event)
 	{
-		RunsafePlayer victim = event.getEntity();
-		this.output.write("Detected player death: " + victim.getName());
-		if (victim.getLastDamageCause() instanceof RunsafeEntityDamageByEntityEvent)
-		{
-			this.output.write("The last damage cause was from an entity");
-			RunsafeEntityDamageByEntityEvent damage = (RunsafeEntityDamageByEntityEvent) victim.getLastDamageCause();
-			if (damage.getDamageActor() instanceof RunsafePlayer)
-			{
-				RunsafePlayer attacker = (RunsafePlayer) damage.getDamageActor();
-				this.output.write("Attacker was a player: " + attacker.getName());
-				this.output.write("Attackers health: " + attacker.getHealth());
-				if (attacker.getHealth() == 1)
-					this.award(attacker);
-			}
-		}
+		RunsafePlayer killer = event.getEntity().getKiller();
+		if (killer != null && killer.getHealth() == 1)
+			this.award(killer);
 	}
-
-	private IOutput output;
 }
