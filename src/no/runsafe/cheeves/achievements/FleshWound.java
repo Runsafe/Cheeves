cb1b0152f1b140ca40860dbad51961eb10338299
Kruithne <kruithne@gmail.com>
2013-06-19 18:23:11 +0100

    [Debug] Output FleshWound stages.

--- src/no/runsafe/cheeves/achievements/FleshWound.java
+++ src/no/runsafe/cheeves/achievements/FleshWound.java
@@ -3,6 +3,7 @@ package no.runsafe.cheeves.achievements;
 import no.runsafe.cheeves.Achievement;
 import no.runsafe.cheeves.AchievementHandler;
 import no.runsafe.cheeves.Achievements;
+import no.runsafe.framework.api.IOutput;
 import no.runsafe.framework.api.event.player.IPlayerDeathEvent;
 import no.runsafe.framework.minecraft.event.entity.RunsafeEntityDamageByEntityEvent;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerDeathEvent;
@@ -10,9 +11,10 @@ import no.runsafe.framework.minecraft.player.RunsafePlayer;
 
 public class FleshWound extends Achievement implements IPlayerDeathEvent
 {
-	public FleshWound(AchievementHandler achievementHandler)
+	public FleshWound(AchievementHandler achievementHandler, IOutput output)
 	{
 		super(achievementHandler);
+		this.output = output;
 	}
 
 	@Override
@@ -37,15 +39,21 @@ public class FleshWound extends Achievement implements IPlayerDeathEvent
 	public void OnPlayerDeathEvent(RunsafePlayerDeathEvent event)
 	{
 		RunsafePlayer victim = event.getEntity();
+		this.output.write("Detected player death: " + victim.getName());
 		if (victim.getLastDamageCause() instanceof RunsafeEntityDamageByEntityEvent)
 		{
+			this.output.write("The last damage cause was from an entity");
 			RunsafeEntityDamageByEntityEvent damage = (RunsafeEntityDamageByEntityEvent) victim.getLastDamageCause();
 			if (damage.getDamageActor() instanceof RunsafePlayer)
 			{
 				RunsafePlayer attacker = (RunsafePlayer) damage.getDamageActor();
+				this.output.write("Attacker was a player: " + attacker.getName());
+				this.output.write("Attackers health: " + attacker.getHealth());
 				if (attacker.getHealth() == 1)
 					this.award(attacker);
 			}
 		}
 	}
+
+	private IOutput output;
 }
