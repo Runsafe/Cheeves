a3f0c3521f8b72e38781b79fe9597fd4a8450c80
Kruithne <kruithne@gmail.com>
2013-06-25 10:01:31 +0100

    Actually use the server first achievement class where needed

--- src/no/runsafe/cheeves/achievements/ServerFirstMyBrainHurts.java
+++ src/no/runsafe/cheeves/achievements/ServerFirstMyBrainHurts.java
@@ -1,12 +1,12 @@
 package no.runsafe.cheeves.achievements;
 
-import no.runsafe.cheeves.Achievement;
 import no.runsafe.cheeves.AchievementHandler;
 import no.runsafe.cheeves.Achievements;
+import no.runsafe.cheeves.ServerFirstAchievement;
 import no.runsafe.framework.api.event.player.IPlayerCustomEvent;
 import no.runsafe.framework.minecraft.event.player.RunsafeCustomEvent;
 
-public class ServerFirstMyBrainHurts extends Achievement implements IPlayerCustomEvent
+public class ServerFirstMyBrainHurts extends ServerFirstAchievement implements IPlayerCustomEvent
 {
 	public ServerFirstMyBrainHurts(AchievementHandler achievementHandler)
 	{
