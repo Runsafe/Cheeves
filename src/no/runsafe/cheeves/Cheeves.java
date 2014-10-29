cb4e9f3fcbfa44c9b3b92427bf667cf2c58bc2d1
ExposedGun316 <exposedgun316@live.co.uk>
2014-10-29 09:08:22 +0000

    Added HuckleBerry Beta achievement

--- src/no/runsafe/cheeves/Cheeves.java
+++ src/no/runsafe/cheeves/Cheeves.java
@@ -82,6 +82,7 @@ public class Cheeves extends RunsafePlugin
 		addComponent(LandHo.class);
 		addComponent(DeadBone.class);
 		addComponent(BelovedFlower.class);
+		addComponent(HuckleBerryBeta.class);
 
 		addComponent(AchievementFinder.class);
 
