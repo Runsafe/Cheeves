9eb041f91e9105d760231b129b39016d92e2dd4c
ExposedGun316 <exposedgun316@live.co.uk>
2015-02-22 20:17:10 +0000

    Added achievement called Master Fighter

--- src/no/runsafe/cheeves/Cheeves.java
+++ src/no/runsafe/cheeves/Cheeves.java
@@ -87,6 +87,7 @@ public class Cheeves extends RunsafePlugin
 		addComponent(ServerFirstMasterTreasureHunter.class);
 		addComponent(MasterTreasureHunter.class);
 		addComponent(HuckleberryBeta.class);
+		addComponent(MasterFighter.class);
 
 		addComponent(AchievementFinder.class);
 
