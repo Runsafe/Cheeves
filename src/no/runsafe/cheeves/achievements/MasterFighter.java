2be105e4fc9ddaa0ef2fc8bc2789769d1687cc6e
ExposedGun316 <exposedgun316@live.co.uk>
2015-02-24 20:16:36 +0000

    Update MasterFighter.java

--- src/no/runsafe/cheeves/achievements/MasterFighter.java
+++ src/no/runsafe/cheeves/achievements/MasterFighter.java
@@ -16,7 +16,7 @@ public class MasterFighter extends Achievement implements IPlayerCustomEvent
 	@Override
 	public String getAchievementName()
 	{
-		return "It's Lonely At The Top";
+		return "It's Lonely at the Top";
 	}
 
 	@Override
