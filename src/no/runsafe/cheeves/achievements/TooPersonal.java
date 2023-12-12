80fdc322253b967196232c3b9b6ca054eadd8350
Centralan <Jessegauthier070296@gmail.com>
2023-12-11 21:33:00 -0500

    Create TooPersonal.java

---
+++ src/no/runsafe/cheeves/achievements/TooPersonal.java
@@ -0,0 +1,40 @@
+package no.runsafe.cheeves.achievements;
+
+import no.runsafe.cheeves.Achievement;
+import no.runsafe.cheeves.AchievementHandler;
+import no.runsafe.cheeves.Achievements;
+import no.runsafe.framework.api.event.player.IPlayerCustomEvent;
+import no.runsafe.framework.minecraft.event.player.RunsafeCustomEvent;
+
+public class TooPersonal extends Achievement implements IPlayerCustomEvent
+{
+	public TooPersonal(AchievementHandler handler)
+	{
+		super(handler);
+	}
+
+	@Override
+	public String getAchievementName()
+	{
+		return "Too Personal";
+	}
+
+	@Override
+	public String getAchievementInfo()
+	{
+		return "Made DOG a little to uncomfortable.";
+	}
+
+	@Override
+	public int getAchievementID()
+	{
+		return Achievements.TOO_PERSONAL.ordinal();
+	}
+
+	@Override
+	public void OnPlayerCustomEvent(RunsafeCustomEvent event)
+	{
+		if (event.getEvent().equals("runsafe.dog.uncomfortable"))
+			award(event.getPlayer());
+	}
+}
