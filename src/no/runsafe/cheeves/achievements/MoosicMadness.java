862b1fcaba14fc35fa15b166839b025739d880f8
Centralan <Jessegauthier070296@gmail.com>
2024-02-06 20:53:13 -0500

    Create MoosicMadness.java

---
+++ src/no/runsafe/cheeves/achievements/MoosicMadness.java
@@ -0,0 +1,31 @@
+package no.runsafe.cheeves.achievements;
+
+import no.runsafe.cheeves.Achievement;
+import no.runsafe.cheeves.AchievementHandler;
+import no.runsafe.cheeves.Achievements;
+
+public class MoosicMadness extends Achievement
+{
+	public MoosicMadness(AchievementHandler achievementHandler)
+	{
+		super(achievementHandler);
+	}
+
+	@Override
+	public String getAchievementName()
+	{
+		return "Moosic Madness";
+	}
+
+	@Override
+	public String getAchievementInfo()
+	{
+		return "Contribute to endless Moosic library of Runsafe!";
+	}
+
+	@Override
+	public int getAchievementID()
+	{
+		return Achievements.MOOSIC_MADNESS.ordinal();
+	}
+}
